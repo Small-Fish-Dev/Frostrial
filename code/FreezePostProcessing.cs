d0b5f4b948e97ad66dec560368eee8e34bae8d65
Luke T <luke.tedd@gmail.com>
2021-12-19 20:27:02 +0000

    testing pp shader

---
+++ code/FreezePostProcessing.cs
@@ -0,0 +1,44 @@
+﻿using Sandbox;
+using System;
+
+
+namespace Sandbox
+{
+
+	public class FreezePostProcessEffect : BasePostProcess
+	{
+		public static Material PostProcessMaterial;
+
+		public class FreezeSettings
+		{
+			internal BasePostProcess parent;
+
+			internal bool _enabled;
+
+
+			public float FreezeStrength
+			{
+				set
+				{
+					parent.Set( "freeze.strength", in value );
+				}
+			}
+
+		}
+		public FreezeSettings Freeze = new FreezeSettings();
+
+		public FreezePostProcessEffect()
+		{
+			PostProcessMaterial = Material.Load( "materials/postprocessing/postprocess_freeze.vmat" );
+			Freeze.parent = this;
+		}
+
+		public override void Render()
+		{
+			if ( PostProcessMaterial == null ) return;
+
+			Sandbox.Render.Material = PostProcessMaterial;
+			RenderScreenQuad();
+		}
+	}
+}
