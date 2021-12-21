558f7385b1d4a09f1b674acde13d9c01c60177a0
yuberee <59583743+yuberee@users.noreply.github.com>
2021-12-21 03:07:29 +0100

    Hut interior

--- code/Hut.cs
+++ code/Hut.cs
@@ -9,6 +9,8 @@ namespace Frostrial
 	public partial class Hut : AnimEntity
 	{
 
+		[Net] PointLightEntity light { get; set; }
+
 		public override void Spawn()
 		{
 
@@ -22,7 +24,15 @@ namespace Frostrial
 			Game current = Game.Current as Game;
 			current.Hut = this;
 
-			new ModelEntity( "models/randommodels/campfire.vmdl" ).Position = Position + Vector3.Up * 12;
+			var fire = new ModelEntity( "models/randommodels/campfire.vmdl" );
+			fire.Position = Position + Vector3.Up * 12;
+			fire.EnableShadowCasting = false;
+
+			light = new PointLightEntity();
+			light.Position = Position + Vector3.Up * 16;
+			light.Color = Color.Orange;
+			light.DynamicShadows = true;
+
 			ModelEntity floor = new ModelEntity( "models/randommodels/cabin_floor.vmdl" );
 			floor.Position = Position;
 			floor.Rotation = Rotation.FromYaw( -90 );
@@ -48,6 +58,8 @@ namespace Frostrial
 			var distance = player.Position.Distance( this.Position );
 
 			RenderColor = RenderColor.WithAlpha( 1 - (startFadeDistance - distance ) / endFadeDistance );
+
+			light.SetLightBrightness( 20 + (float)Math.Cos( (float)Time.Now * 25 ) * 2 * ( 1 + Time.Now % 1 ) ); // Acceptable flickering
 
 		}
 
