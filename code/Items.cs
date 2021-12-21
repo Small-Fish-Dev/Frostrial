611c541ca30478899f7f45f0deaeb15c446706af
yuberee <59583743+yuberee@users.noreply.github.com>
2021-12-21 22:32:41 +0100

    Finalised Items. Fixed annoying hint text error. New work particle

--- code/Items.cs
+++ code/Items.cs
@@ -8,15 +8,64 @@ namespace Frostrial
 	partial class Player : Sandbox.Player
 	{
 
+		[Net] public int Baits { get; set; } = 1;
+		[Net] public int Campfires { get; set; } = 1;
 		[Net] public bool ItemsOpen { get; set; } = false;
+		[Net] public bool PlacingCampfire { get; set; } = false;
 
 		public void HandleItems()
 		{
 
+			if ( PlacingCampfire )
+			{
+
+				var mousePos = MouseWorldPosition;
+				var playerDistance = mousePos.Distance( Position );
+				bool canPlace = !Game.IsNearEntity( mousePos, 30f ) && playerDistance <= 100f && playerDistance >= 45f;
+
+				if ( IsClient )
+				{
+
+					DebugOverlay.Circle( MouseWorldPosition + Vector3.Up, Rotation.FromPitch( 90 ), 20f, new Color( canPlace ? 0 : 1, canPlace ? 1 : 0, 0, 0.1f ), true, 0.1f);
+
+				}
+				else
+				{
+
+					if ( Input.Pressed( InputButton.Attack1 ) )
+					{
+
+						if ( canPlace )
+						{
+
+							PlacingCampfire = false;
+							BlockMovement = false;
+
+							Campfire ent = new Campfire();
+							ent.Position = MouseWorldPosition;
+
+							Campfires--;
+
+						}
+						else
+						{
+
+							PlacingCampfire = false;
+							BlockMovement = false;
+							Hint( "I can't put that there...", 2 );
+
+						}
+
+					}
+
+				}
+
+			}
+
 		}
 
 		[ServerCmd]
-		public static void ItemSelected()
+		public static void CloseItems()
 		{
 
 			Player player = ConsoleSystem.Caller.Pawn as Player;
@@ -24,8 +73,61 @@ namespace Frostrial
 			player.ItemsOpen = false;
 			player.BlockMovement = false;
 
-			Campfire fire = new Campfire();
-			fire.Position = player.Position;
+		}
+
+		[ServerCmd]
+		public static void BaitSelected()
+		{
+
+			Player player = ConsoleSystem.Caller.Pawn as Player;
+
+			if ( player.Baits > 0 )
+			{
+
+				player.ItemsOpen = false;
+				player.BlockMovement = false;
+
+				player.Baits--;
+
+				player.Hint( "Oops, some slipped out", 1.5f );
+
+				Particles.Create( "particles/worms_particle.vpcf", player.Position );
+
+			}
+			else
+			{
+
+				player.ItemsOpen = false;
+				player.BlockMovement = false;
+
+				player.Hint( "I don't have any of those...", 2 );
+
+			}
+
+		}
+
+		[ServerCmd]
+		public static void CampfireSelected()
+		{
+
+			Player player = ConsoleSystem.Caller.Pawn as Player;
+
+			if ( player.Campfires > 0 )
+			{
+
+				player.ItemsOpen = false;
+				player.PlacingCampfire = true;
+
+			}
+			else
+			{
+
+				player.ItemsOpen = false;
+				player.BlockMovement = false;
+
+				player.Hint( "I don't have any of those...", 2 );
+
+			}
 
 		}
 
@@ -34,8 +136,10 @@ namespace Frostrial
 	public class Items : Panel
 	{
 
-		Label button1;
-		Label button2;
+		Button button1;
+		Button button2;
+		Label text1;
+		Label text2;
 
 		public Items()
 		{
@@ -47,17 +151,35 @@ namespace Frostrial
 			button1 = itemPanel.Add.Button( "", "button", () =>
 			{
 
-				Player.ItemSelected();
+				Player.BaitSelected();
 
-			} ).Add.Label( "Bait", "title" );
+			} );
+			
+			text1 = button1.Add.Label( "Bait", "title" );
 
 			button2 = itemPanel.Add.Button( "", "button", () =>
 
 			{
 
-				Player.ItemSelected();
+				Player.CampfireSelected();
 
-			} ).Add.Label( "Campfire", "title" );
+			} );
+			
+			text2 = button2.Add.Label( "Campfire", "title" );
+
+		}
+
+		public override void OnButtonEvent( ButtonEvent e )
+		{
+
+			base.OnButtonEvent( e );
+
+			if ( e.Pressed == true && e.Button == "mouseright")
+			{
+
+				Player.CloseItems();
+
+			}
 
 		}
 
@@ -68,8 +190,8 @@ namespace Frostrial
 			Parent.Style.PointerEvents = player.ItemsOpen ? "all" : "visible";
 			Style.Opacity = player.ItemsOpen ? 1 : 0;
 
-			button1.Text = $"Bait ({0})"; // TODO Track amount of items
-			button2.Text = $"Campfire ({0})";
+			text1.Text = $"Bait ({player.Baits})"; // TODO Track amount of items
+			text2.Text = $"Campfire ({player.Campfires})";
 
 		}
 
