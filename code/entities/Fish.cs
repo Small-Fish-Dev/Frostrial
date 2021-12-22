36e4cd263ad365d4ac6a71d06730b70f04b6518f
yuberee <59583743+yuberee@users.noreply.github.com>
2021-12-22 03:30:13 +0100

    Fishes move lol

--- code/entities/Fish.cs
+++ code/entities/Fish.cs
@@ -1,5 +1,6 @@
 ﻿using Sandbox;
 using System;
+using System.Collections.Generic;
 
 namespace Frostrial
 {
@@ -10,7 +11,10 @@ namespace Frostrial
 		[Net] public string Species { get; set; } = "goldfish";
 		[Net] public float Size { get; set; } = 0.1f; // Meters
 		[Net] public bool Variant { get; set; } = false;
-		[Net] public FishSpawner Spawner { get; set; }
+		public FishSpawner Spawner { get; set; }
+		public IList<Fish> FishList;
+		[Net] RealTimeUntil growth { get; set; }
+		RealTimeUntil nextMove = 0f;
 
 		public override void Spawn()
 		{
@@ -18,14 +22,43 @@ namespace Frostrial
 			base.Spawn();
 
 			SetModel( "models/fishes/fishshadow.vmdl" );
-
 			EnableShadowCasting = false;
+			growth = 1;
+
+			Velocity = Vector3.Forward * 50f;
 
 		}
 
 		[Event.Tick.Server]
 		public void OnTick()
 		{
+
+			if ( nextMove < 0f )
+			{
+
+				if ( Position.Distance( Spawner.Position ) > Spawner.Range )
+				{
+
+					Velocity = ( Spawner.Position - Position).Normal * 70f;
+
+				}
+				else
+				{
+
+					Velocity = new Vector3( Rand.Float( 2f ) - 1f, Rand.Float( 2f ) - 1f, 0f ).Normal * Rand.Float( 20f, 60f );
+
+				}
+
+				nextMove = Rand.Float( 2, 7 );
+
+			}
+
+			Scale = Size * 3 * Math.Min( 1 - growth, 1 );
+
+			Position += Rotation.Forward * Velocity.Length * Time.Delta;
+
+			Rotation rotation = Velocity.EulerAngles.ToRotation();
+			Rotation = Rotation.Slerp( Rotation, rotation, 2 * Time.Delta );
 
 		}
 
--- code/mechanics/Warmth.cs
+++ code/mechanics/Warmth.cs
@@ -9,7 +9,7 @@ namespace Frostrial
 
 		[Net] public float Warmth { get; set; } = 1f;
 		[Net] public float ColdMultiplier { get; set; } = 1f; // Negative will recover warmth
-		[Net] public float BaseColdSpeed { get; set; } = 30f; // Total seconds to perish in neutral conditions ( Standing on Dirt and not moving )
+		[Net] public float BaseColdSpeed { get; set; } = 60f; // Total seconds to perish in neutral conditions ( Standing on Dirt and not moving )
 		[Net] public bool SuffersCold { get; set; } = true;
 
 		public void HandleWarmth()
