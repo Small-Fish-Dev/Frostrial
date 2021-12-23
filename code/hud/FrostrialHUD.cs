bd957b8730ef420a4ef3f13bc1835eae8900b0bf
yuberee <59583743+yuberee@users.noreply.github.com>
2021-12-23 12:29:13 +0100

    Fixed UI again. Title and Subtitle WIP

--- code/hud/FrostrialHUD.cs
+++ code/hud/FrostrialHUD.cs
@@ -28,8 +28,8 @@ namespace Frostrial
 			var player = Local.Pawn as Player;
 			var hut = current.HutEntity;
 			var hutScreen = hut.Position.ToScreen();
-			var left = MathX.Clamp( hutScreen.x, 0.2f, 0.8f );
-			var top = MathX.Clamp( hutScreen.y, 0.1f, 1f);
+			var left = MathX.Clamp( hutScreen.x, 0.21f, 0.75f );
+			var top = MathX.Clamp( hutScreen.y + 0.1f, 0.1f, 1.1f);
 
 			Style.Left = Length.Pixels( ( left - 0.5f ) * 3000 );
 			Style.Top = Length.Pixels( ( top - 0.1f ) * 800 );
@@ -42,7 +42,7 @@ namespace Frostrial
 
 			Style.Opacity = MathX.Clamp( ( player.Position.Distance( hut.Position ) - baseDistance ) / baseDistance , 0, 1f ) * baseOpacity * ( baseOpacity + 1 / baseOpacity * dangerLevel );
 
-			var rotation = -MathX.RadianToDegree( (float)Math.Atan2( 0.5f - left , 0.5 - top ) );
+			var rotation = -MathX.RadianToDegree( (float)Math.Atan2( 0.5f - hutScreen.x, 0.5 - hutScreen.y ) );
 
 			var arrowRotate = new PanelTransform();
 			arrowRotate.AddRotation( 0, 0, rotation );
@@ -176,6 +176,30 @@ namespace Frostrial
 
 	}
 
+	public class Curtains : Panel
+	{
+
+		Panel curtainsPanel;
+		Label curtainsTitle;
+		Label curtainsSubtitle;
+		public Curtains()
+		{
+
+			curtainsPanel = Add.Panel( "Curtains" ).Add.Panel( "MapContainer" );
+			curtainsTitle = curtainsPanel.Add.Panel( "titleContainer" ).Add.Label( "Wake Up", "curtainsTitle" );
+			curtainsSubtitle = curtainsPanel.Add.Panel( "subtitleContainer" ).Add.Label( "Joe Biden, wake up", "curtainsSubtitle" );
+
+		}
+
+		public override void Tick()
+		{
+
+
+
+		}
+
+	}
+
 	public partial class FrostrialHUD : Sandbox.HudEntity<RootPanel>
 	{
 
@@ -194,6 +218,7 @@ namespace Frostrial
 			RootPanel.AddChild<Hint>();
 			RootPanel.AddChild<Map>();
 			RootPanel.AddChild<HutIndicator>();
+			RootPanel.AddChild<Curtains>();
 			RootPanel.AddChild<Items>();
 			RootPanel.AddChild<Shop>();
 
