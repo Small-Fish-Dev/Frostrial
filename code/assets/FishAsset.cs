f35d887de96277401d9a36da0a334c0c66c5d19c
yuberee <59583743+yuberee@users.noreply.github.com>
2022-02-01 02:27:35 +0100

    Fish Asset INITIAL

---
+++ code/assets/FishAsset.cs
@@ -0,0 +1,57 @@
+﻿using Sandbox;
+using System.Collections.Generic;
+
+namespace Frostrial
+{
+	[Library("fish"), AutoGenerate]
+	public partial class FishAsset : Asset
+	{
+		public static IReadOnlyList<FishAsset> All => _all;
+		internal static List<FishAsset> _all = new();
+
+		[Property]
+		public string FishName { get; set; }
+		[Property]
+		public string CommonName { get; set; }
+		[Property]
+		public string ScientificName { get; set; }
+		[Property]
+		public string Description { get; set; }
+		[Property, ResourceType( "vmdl" )]
+		public string Model { get; set; }
+		[Property, ResourceType( "png" )]
+		public string Preview { get; set; }
+		[Property]
+		public float Size { get; set; }
+		[Property]
+		public float Rarity { get; set; }
+		[Property]
+		public string VariantName { get; set; }
+		[Property]
+		public string VariantCommonName { get; set; }
+		[Property]
+		public string VariantScientificName { get; set; }
+		[Property]
+		public string VariantDescription { get; set; }
+		[Property]
+		public string VariantSkin { get; set; }
+		[Property, ResourceType( "png" )]
+		public string VariantPreview { get; set; }
+		[Property]
+		public int Weighted1 { get; set; }
+		[Property]
+		public int Weighted2 { get; set; }
+		[Property]
+		public int Weighted3 { get; set; }
+
+
+
+		protected override void PostLoad()
+		{
+			base.PostLoad();
+
+			if ( !_all.Contains( this ) )
+				_all.Add( this );
+		}
+	}
+}
---
+++ fish/pike.fish
@@ -0,0 +1,23 @@
+<!-- kv3 encoding:text:version{e21c7f3c-8a33-41c5-9977-a76d3a32aa0d} format:generic:version{7412167c-06e9-4698-aff2-e63eb59037e7} -->
+{
+	data = 
+	{
+		fishname = "Pike"
+		commonname = "Muskellunge"
+		scientificname = "Esox Masquinongy"
+		description = "He looks happy."
+		model = resource:"models/fishes/pike/pike.vmdl"
+		preview = "ui/fishes/pike.jpg"
+		size = 0.6
+		rarity = 0.5
+		variantname = "Pike Chain"
+		variantcommonname = "Chain Pickerel"
+		variantscientificname = "Esox Niger"
+		variantdescription = "What did they mean by that\?"
+		variantskin = "chain"
+		variantpreview = "ui/fishes/pike_chain.jpg"
+		weighted1 = 4
+		weighted2 = 30
+		weighted3 = 25
+	}
+}
